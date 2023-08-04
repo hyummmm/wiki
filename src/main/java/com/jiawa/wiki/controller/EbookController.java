a54f4a98eed1842107d5c35683a7e76a551572db
hyummmm <799179889@qq.com>
2023-08-04 22:40:59 +0800

    3-6 增加通用返回类CommonResponse，以后所有后端接口的返回值全部都是CommonResponse类型

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.response.CommonResponse;
 import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,12 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResponse list() {
+        CommonResponse<List<Ebook>> response = new CommonResponse<>();
+        List<Ebook> list =  ebookService.list();
+        response.setContent(list);
+        return response;
+
     }
 
 }
