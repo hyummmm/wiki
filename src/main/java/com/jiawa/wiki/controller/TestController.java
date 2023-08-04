aae370309810fe083fd38727bfabf90acf31ca6f
hyummmm <799179889@qq.com>
2023-08-04 21:14:05 +0800

    2-9 集成热部署

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.jiawa.wiki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Word!";
+        return "Hello Word!" + testHello;
     }
 }
