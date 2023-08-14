8185c9258f00d5234b8e81e320213770bb6f95ee
hyummmm <799179889@qq.com>
2023-08-14 15:27:14 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    wiki ©2023 Created by Hyummmm
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
